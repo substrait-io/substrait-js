671348d4af17773cb381c60e6aff3e91181e6b9a
Weston Pace <weston.pace@gmail.com>
2022-05-27 18:23:11 -1000

    Initial commit

---
+++ scripts/download.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const download = require('download-git-repo')
+
+const { version } = require('../package.json');
+
+console.log(`Downloading and extracting Substrait version ${version}`);
+
+download(`substrait-io/substrait#v${version}`, 'substrait-download', {}, function (err) {
+    if (err) {
+        console.log('An error occurred downloading the Substrait repo:');
+        console.log();
+        console.log(err);
+    }
+});
